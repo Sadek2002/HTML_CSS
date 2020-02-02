0f24946106e82a96afb0821e4b952651683d4161
Sadek2002 <sharkalmousawi@Live.com>
2020-02-02 18:15:27 +0100

    Website Gta 5

---
+++ H05/login2.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="http://www.savatarian.com/html5/form.php" method="post">
+    <ul>
+        <li>
+            <h2>Voorbeeldformulier</h2>
+        </li>
+        <li>
+            <label for="voornaam">Voornaam:</label>
+            <input type="text" name="voornaam" id="voornaam">
+        </li>
+        <li>
+            <label for="achternaam">Achternaam:</label>
+            <input type="text" name="achternaam" id="achternaam">
+        </li>
+    </ul>
+</form>
+</body>
+</html>
